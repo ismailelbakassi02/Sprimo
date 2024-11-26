4c895c4e0b487ad87284d9b9fc3606cb48808c0e
ismail el bakassi <ismailelbakassi@gmail.com>
2024-11-26 11:35:48 +0100

    2nd commit

--- main.py
+++ main.py
@@ -7,17 +7,27 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Sprimo')
 
 #charji les images
-player=pygame.image.load('assets/player.png').convert_alpha()
+character=pygame.image.load('assets/character.png').convert_alpha()
 backgroung=pygame.image.load('assets/bg.png').convert_alpha()
 
-#player class 
-class Player():
+#character class 
+class Character():
     def __init__(self,x,y):
-     self.image=player
+     self.image=pygame.transform.scale(character,(60,60))
+     self.rect=self.image.get_rect()
+     self.rect.center=(x,y)
+    def draw (self):
+        screen.blit(self.image,self.rect)
+        pygame.draw.rect(screen,(255,0,0),self.rect,2)
+
+#Les object  
+character=Character(WIDTH/2,HEIGHT-100)
+
 #loop dyal lgame bach tb9a khdama 
 run = True
 while run:
     screen.blit(backgroung,(-100,-150))
+    character.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
